e6399fabe9eda6b559ba81c0bd2e567be50ac99b
kamotetopskie <kamotetopskie@gmail.com>
2020-12-31 21:45:14 +0800

    - Fixed styling

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,11 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography, Button, IconButton,  } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core';
 import { Menu as MenuIcon, Search as SearchIcon } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
   menuButton: {
     marginRight: theme.spacing(2),
   },
@@ -18,7 +15,7 @@ const TopNav = () => {
     const classes = useStyles();
 
     return (
-      <div className={classes.root}>
+      <div>
         <AppBar position="static" style={{backgroundColor:"#FB9F00"}}>
           <Toolbar>
             <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
