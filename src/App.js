2a824aa0b9aecd386df033f163b6708469c35c23
kamotetopskie <kamotetopskie@gmail.com>
2021-01-01 20:53:55 +0800

    - Added Modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import React, { useState } from 'react';
 import './App.css';
 import TopNav from './components/TopNav/TopNav';
 import BottomNav from './components/BottomNav/BottomNav';
 import Content from './components/Content/Content';
+import Details from './components/Details/Details';
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const toggleModal = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className="App">
+      <Details 
+        onToggle={toggleModal}
+        open={open} 
+      />
       <TopNav/>
-      <Content/>
+      <Content 
+        onToggle={toggleModal}
+      />
       <BottomNav/>
     </div>
   );
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,12 +1,12 @@
 import { Button } from '@material-ui/core';
 import milkTea from '../../assets/products/milktea.png'; 
 
-const Content = () => {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
+const Content = ({ onToggle }) => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
   
   return (
     <div style={{backgroundColor:"#EDF2F3", flexGrow: 1, overflowY:"scroll"}}>
-      <div style={{display:"grid", gridTemplateColumns:"1fr 1fr", margin: ".5em", gridGap: ".5em"}}>
+      <div style={{display:"grid", gridTemplateColumns:"1fr 1fr 1fr", margin: ".5em", gridGap: ".5em"}}>
         {arr.map(() => 
           <div style={{
             backgroundColor:"#fff", 
@@ -14,14 +14,15 @@ const Content = () => {
             WebkitBoxShadow: "0px 0px 9px 1px rgba(0,0,0,0.2)", 
             MozBoxShadow: "0px 0px 9px 1px rgba(0,0,0,0.2)", 
             boxShadow: "0px 0px 9px 1px rgba(0,0,0,0.2)"}}
+            onClick={onToggle}
           >
             <p style={{textAlign:"center", margin: "10px 0 5px 0"}}>
-              <img src={milkTea} alt="Product"/>
+              <img src={milkTea} alt="Product" style={{height:70}}/>
             </p>
             <h5 style={{textAlign:"center", margin: 0}}>Match Milk Tea</h5>
             <p style={{textAlign:"center", margin: "5px 0"}}>₱ 120.00</p>
             <p style={{textAlign:"center", margin: "10px 0"}}>
-              <Button variant="contained" style={{backgroundColor:"#FB9F00", color:"#fff"}}>Add to cart</Button>
+              <Button variant="contained" style={{backgroundColor:"#FB9F00", color:"#fff", padding: "3px 7px", fontSize:8}}>Add to cart</Button>
             </p>
           </div>
         )}
